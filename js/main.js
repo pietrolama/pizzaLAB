e71e59e434549d2b88e1fc1fe70643268f04b178
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-06 19:36:30 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// js/main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('toggle');
+    });
+
+    // Chiudi il menu quando clicchi su un link (opzionale)
+    const navItems = document.querySelectorAll('.nav-links li a');
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navLinks.classList.remove('active');
+            hamburger.classList.remove('toggle');
+        });
+    });
+});
