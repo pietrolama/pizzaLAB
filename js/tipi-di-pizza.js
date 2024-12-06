6a7ee8449de7de886be32b17f130e17831fc906d
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-07 00:47:53 +0100

    Update tipi-di-pizza.js

--- js/tipi-di-pizza.js
+++ js/tipi-di-pizza.js
@@ -1,16 +1,20 @@
+// js/tipi-di-pizza.js
+
 fetch('data/pizze.json')
     .then(response => response.json())
     .then(data => {
-        const container = document.getElementById('pizze-container');
+        const container = document.querySelector('.pizza-container');
         data.forEach(pizza => {
             const section = document.createElement('section');
             section.innerHTML = `
-                <h2>${pizza.nome}</h2>
                 <img src="${pizza.immagine}" alt="${pizza.nome}" class="pizza-img">
-                <p>${pizza.descrizione}</p>
-                <ul>
-                    ${pizza.caratteristiche.map(car => `<li>${car}</li>`).join('')}
-                </ul>
+                <div class="pizza-content">
+                    <h2>${pizza.nome}</h2>
+                    <p>${pizza.descrizione}</p>
+                    <ul>
+                        ${pizza.caratteristiche.map(car => `<li>${car}</li>`).join('')}
+                    </ul>
+                </div>
             `;
             container.appendChild(section);
         });
