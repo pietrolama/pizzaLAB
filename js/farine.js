5a70d48b63484a36a7eeaa5b63b0e2279842a2e7
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-09 15:08:07 +0100

    Update farine.js

--- js/farine.js
+++ js/farine.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data/farine.json')
+// js/farine.js
+document.addEventListener('DOMContentLoaded', () => { 
+    fetch('/data/farine.json') // Percorso assoluto
         .then(response => response.json())
         .then(data => {
             const container = document.getElementById('farine-container');
@@ -11,17 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const dettagliUrl = `dettagli-farina.html?id=${farina.id}`;
 
+                // Controllo se 'ingredienti' esiste ed è un array
+                const ingredienti = Array.isArray(farina.ingredienti) 
+                    ? farina.ingredienti.join(', ') 
+                    : 'N/A'; // O qualsiasi valore predefinito desideri
+
                 section.innerHTML = `
-                    <h2>${farina.titolo}</h2>
                     <img src="${farina.immagine}" alt="${farina.titolo}" class="farina-img">
-                    <p>${farina.descrizione}</p>
-                    <h3>Caratteristiche</h3>
-                    <ul>
-                        ${farina.caratteristiche.map(car => `<li>${car}</li>`).join('')}
-                    </ul>
-                    <p><strong>Contenuto di Glutine:</strong> ${farina.contenuto_glutine}</p>
-                    <p><strong>Uso:</strong> ${farina.uso}</p>
-                    <p><strong>Procedimento:</strong> ${farina.procedimento}</p>
+                    <div class="item-content">
+                        <h2>${farina.titolo}</h2>
+                        <p>${farina.descrizione}</p>
+                        <h3>Caratteristiche</h3>
+                        <ul>
+                            ${farina.caratteristiche.map(car => `<li>${car}</li>`).join('')}
+                        </ul>
+                        <p><strong>Contenuto di Glutine:</strong> ${farina.contenuto_glutine}</p>
+                        <p><strong>Uso:</strong> ${farina.uso}</p>
+                        <p><strong>Procedimento:</strong> ${farina.procedimento}</p>
+                        ${farina.ingredienti ? `<p><strong>Ingredienti:</strong> ${ingredienti}</p>` : ''}
+                    </div>
                 `;
                 container.appendChild(section);
             });
