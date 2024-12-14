c38d43289db061ecdca4bb47cc28370b9e89de3d
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-14 10:14:36 +0100

    Create cookies.js

---
+++ js/cookies.js
@@ -0,0 +1,44 @@
+// Configurazione tarteaucitron.js
+tarteaucitron.init({
+    "privacyUrl": "/privacy", // URL della pagina di privacy policy
+    "hashtag": "#tarteaucitron", 
+    "cookieName": "tarteaucitron", 
+    "orientation": "bottom", 
+    "showAlertSmall": true, 
+    "cookieslist": true, 
+    "adblocker": false, 
+    "AcceptAllCta": true, 
+    "highPrivacy": false, 
+    "handleBrowserDNTRequest": false, 
+    "removeCredit": true, 
+    "moreInfoLink": true, 
+    "useExternalCss": false 
+});
+
+// Configurazione Google Analytics
+tarteaucitron.services.googleanalytics = {
+    "key": "googleanalytics",
+    "type": "analytic",
+    "name": "Google Analytics",
+    "uri": "https://policies.google.com/privacy",
+    "needConsent": true,
+    "cookies": ["_ga", "_gid"],
+    "js": function () {
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments);
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m);
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'G-1CV0W5QPKV', 'auto');
+        ga('send', 'pageview');
+    },
+    "fallback": function () {
+        console.log("Google Analytics non attivo");
+    }
+};
