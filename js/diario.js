aabaffe922df02b0314c2972c9de129e9cd30adc
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-24 19:01:46 +0100

    Update diario.js

--- js/diario.js
+++ js/diario.js
@@ -1,38 +1,9 @@
-// Importa Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { app } from "./login.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
-// Configurazione Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyDx2udaOvFXoQP-H2lldGXD268yrZHM0aI",
-    authDomain: "pizzalab-4b769.firebaseapp.com",
-    projectId: "pizzalab-4b769",
-    storageBucket: "pizzalab-4b769.firebasestorage.app",
-    messagingSenderId: "1051118488916",
-    appId: "1:1051118488916:web:b7aeb04695886b1b764cc1",
-    measurementId: "G-2VE5X45NER"
-};
-
-// Inizializza Firebase
-const app = initializeApp(firebaseConfig);
-
-// Inizializza Firebase Auth e Firestore
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Controlla se l'utente è autenticato
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        console.log("Utente autenticato:", user.displayName, user.email, user.uid);
-        caricaFermentazioni(user.uid);
-    } else {
-        console.error("Nessun utente autenticato. Reindirizzamento al login...");
-        alert("Devi essere autenticato per accedere al diario.");
-        window.location.href = "login.html";
-    }
-});
-
 
 // Aggiungi una nuova fermentazione
 document.getElementById("fermentazione-form").addEventListener("submit", async (e) => {
