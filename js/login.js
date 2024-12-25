763b2d18d3e06d0fbf77093872eafc9baa9788c4
pietrolama <32061233+pietrolama@users.noreply.github.com>
2024-12-25 12:30:53 +0100

    Update login.js

--- js/login.js
+++ js/login.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
     measurementId: "G-2VE5X45NER"
 };
 
-export const app = initializeApp(firebaseConfig);
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
